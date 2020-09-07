564ef3f0a54d6c4eb732251a90a4dd3908625554
Yangerle <1136510804@qq.com>
2020-09-07 10:47:47 +0800

    页面内容初始化隐藏，模态框消失后再显示

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     	backdrop:true,
     	keyboard:false
     });
-    $('#bigworld>div').hide();
     $('[data-toggle=popover]').popover();
     
 });
@@ -53,63 +52,33 @@ $(function () {
     			transform:'rotate('+deg+'deg)'
     		}).data('deg', deg).show();
     	});
+    	$('#bigworld').show();
     });
 // 拖放
-	    var correct=(window.innerWidth-parseInt($('#bigworld').css('width')))/2;
-	        $drag=null;
-	        setI1=null;
-	        setI2=null;
-	        correctTop=0;
-	        correctLeft=0;
-	        clientX=0;
-	        clientY=0;
-	        j=0;
+    var correct=(window.innerWidth-parseInt($('#bigworld').css('width')))/2;
+        $drag=null;
+        setI1=null;
+        setI2=null;
+        correctTop=0;
+        correctLeft=0;
+        clientX=0;
+        clientY=0;
+        j=0;
 	$('#bigworld').on('mousedown', 'div[draggable]', function(event) {
 		event.preventDefault();
 		clientX=event.clientX-correct;
 		clientY=event.clientY;
 		console.log(event.button);
-		// switch(event.type){
-		// 	case "mousedown":
-			if(this.className.indexOf("draggable")){
-				$drag=$(this);
-				correctTop=clientY-parseInt($drag.css('top'));
-				correctLeft=clientX-parseInt($drag.css('left'));
-				j+=1;
+		if(this.className.indexOf("draggable")){
+			$drag=$(this);
+			correctTop=clientY-parseInt($drag.css('top'));
+			correctLeft=clientX-parseInt($drag.css('left'));
+			j+=1;
 
-				$drag.css('z-index',j).siblings('z-index',0);
-				j=j%2;
-				rotate(event);
-				// var i=$drag.data('deg');
-				// if ($drag!==null) {
-				// 	if (event.button==0) {
-				// 		    setI1=setInterval(function () {
-				// 		    i+=1;
-				// 			$drag.css({
-				// 				transform:'rotate('+i+'deg)'
-				// 			}).data('deg', i);	
-				// 		},50);
-				// 	};
-				// 	if (event.button==2) {
-				// 		    setI2=setInterval(function () {
-				// 		    i-=1;
-				// 			$drag.css({
-				// 				transform:'rotate('+i+'deg)'
-				// 			}).data('deg', i);	
-				// 		},50);
-				// 	}
-					   
-				// }
-		    }
-		//     break;
-		//     case "mousemove":
-		//     if ($drag!==null) {
-		//     		$drag.css({
-		//     		top: (clientY-correctTop)+"px",
-		//     		left: (clientX-correctLeft)+"px"
-		//     	});
-		//     };
-		// }
+			$drag.css('z-index',j).siblings('z-index',0);
+			j=j%2;
+			rotate(event);
+	    }
 	});
 	$('body').on('mouseup mousemove', function(event) {
 		event.preventDefault();
@@ -117,17 +86,16 @@ $(function () {
         clearInterval(setI2);
 		switch(event.type){
 			case "mouseup":
-			
-			$drag=null;
-			break;
+				$drag=null;
+				break;
 			case "mousemove":
 			if ($drag!==null) {
 				    clientX=event.clientX-correct;
 				    clientY=event.clientY;
 					$drag.css({
-					top: (clientY-correctTop)+"px",
-					left: (clientX-correctLeft)+"px"
-				});
+						top: (clientY-correctTop)+"px",
+						left: (clientX-correctLeft)+"px"
+					});
 			}
 		}
 		
